f5f33cf1e4eadcff4afaa3736aedad633e89f85d
Andrew Olejnik <aolejnikccsj@outlook.com>
2019-01-22 15:00:01 -0600

    spacing was wrong and now is corrected pp228

--- SportsStores/Controllers/ProductController.cs
+++ SportsStores/Controllers/ProductController.cs
@@ -14,29 +14,32 @@ namespace SportsStores.Controllers
         private IProductRepository repository;
         public int PageSize = 4;
 
-        public ProductController(IProductRepository repo)
-        {
+        public ProductController(IProductRepository repo) {
             repository = repo;
         }
+
         //public ViewResult List()
-          //  => View(repository.Products);
+        //  => View(repository.Products);
 
         public ViewResult List(int productPage = 1)
 
-          =>  View(new ProductsListViewModel
-            {
-                Products = repository.Products
+          => View(new ProductsListViewModel
+          {
+              Products = repository.Products
                                .OrderBy(p => p.ProductID)
                                .Skip((productPage - 1) * PageSize)
                                .Take(PageSize),
-                PagingInfo = new PagingInfo
-                {
-                    CurrentPage = productPage,
-                    ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                }
-            });
+              PagingInfo = new PagingInfo
+              {
+                  CurrentPage = productPage,
+                  ItemsPerPage = PageSize,
+                  TotalItems = repository.Products.Count()
+              }
+          });
 
-        
+
     }
 }
+
+
+
